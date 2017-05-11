4060fee84bb494b346e8a1120114b083ce276dff
chenqp <13542427@qq.com>
2017-05-11 17:33:31 +0800

    添加项目文件。

---
+++ Tests.ImageCutting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.ImageCutting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
